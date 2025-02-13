6d5db02bce679e7a9df8257df68085847570dedc
Andrew Dixon <andrew.dixon@principleone.co.uk>
2025-02-13 10:57:07 +0000

    ML for responses

--- resources/app.py
+++ resources/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 app = Flask(__name__)
 
@@ -17,6 +20,25 @@ def init_db():
     conn.commit()
     cursor.close()
     conn.close()
+
+# Train the model
+def train_model():
+    conn = psycopg2.connect(
+        host="db",
+        database="yourdatabase",
+        user="yourusername",
+        password="yourpassword"
+    )
+    df = pd.read_sql_query("SELECT * FROM preferences", conn)
+    conn.close()
+
+    if not df.empty:
+        X = df['question']
+        y = df['answer']
+        model = RandomForestClassifier()
+        model.fit(X, y)
+        with open('model.pkl', 'wb') as f:
+            pickle.dump(model, f)
 
 @app.route('/ask', methods=['GET'])
 def ask_question():
@@ -41,7 +63,21 @@ def get_answer():
     cursor.close()
     conn.close()
 
+    train_model()
+
     return jsonify({'status': 'success'})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    question = data['question']
+
+    with open('model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    prediction = model.predict([question])
+
+    return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
     init_db()
